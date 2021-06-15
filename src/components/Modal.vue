65a290e37d20fa8e7dff450bb139dfe9f167fb1e
noariuchiha <WAIRECRAFFTERLOUANNE2020>
2021-06-15 18:21:49 +0200

    skyplus

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="modal flex">
     <div class="modal-content">
-      <p>Are you sure you want to exit? Your changes will not be saved?</p>
+      <p>Êtes-vous sûr de vouloir quitter ? Vos modifications ne seront pas enregistrées ?</p>
       <div class="actions flex">
-        <button @click="closeModal" class="purple">Return</button>
-        <button @click="closeInvoice" class="red">Close</button>
+        <button @click="closeModal" class="purple">Retour</button>
+        <button @click="closeInvoice" class="red">Fermer</button>
       </div>
     </div>
   </div>
